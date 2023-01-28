d62cd660e339214ec0d335c5df0f457a62932e6d
ajaysake1992 <ajay.sake.tech@gmail.com>
2023-01-28 21:45:04 +0530

    added receive ethereum payable function

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,8 +2,5 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-    // storage variable
-    uint public funds = 1000;
-    int public counter = -10;
-    uint32 public test = 4294967295;
+    receive() external payable{}
 }
--- truffle-config.js
+++ truffle-config.js
@@ -20,7 +20,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.5.1",
+      version: "0.8.1",
     },
   },
 };
