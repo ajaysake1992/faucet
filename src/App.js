cbeeb3d64f618b0e6f6701cd71f37ca7ebfb0991
ajaysake1992 <ajay.sake.tech@gmail.com>
2023-04-08 13:22:28 +0530

    loaded contract and integrted balance

--- src/App.js
+++ src/App.js
@@ -2,23 +2,28 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Web3 from "web3";
 import detectEthereumProvider from "@metamask/detect-provider";
+import { loadContract } from "./utils/load-contract";
 
 function App() {
   const [web3Api, setWeb3Api] = useState({
     provider: null,
     web3: null,
+    contract: null
   });
 
   const [account, setAccounts] = useState(null);
+  const [balance, setBalance] = useState(0);
 
   useEffect(() => {
     const loadProvider = async () => {
       const provider = await detectEthereumProvider();
+      const contract  = await loadContract("Faucet", provider);
       if (provider) {
         provider.request({ method: "eth_requestAccounts" });
         setWeb3Api({
           web3: new Web3(provider),
           provider,
+          contract
         });
       } else {
         // if the provider is not detected, detectEthereumProvider resolves to null
@@ -29,10 +34,20 @@ function App() {
   }, []);
 
   useEffect(() => {
+    const fetchContractBalance = async() => {
+      const {web3, contract} = web3Api;
+      const balance = await web3.eth.getBalance(contract.address);
+      setBalance(web3.utils.fromWei(balance, "ether"));
+    }
+    web3Api.web3 && fetchContractBalance();
+  }, [web3Api]);
+
+  useEffect(() => {
     const getAccounts = async () => {
       const accounts = await web3Api.web3.eth.getAccounts();
       setAccounts(accounts[0]);
     };
+    console.log(web3Api.web3);
     web3Api.web3 && getAccounts();
   }, [web3Api.web3]);
   
@@ -58,7 +73,7 @@ function App() {
             )}
           </div>
           <div className="balance-view is-size-2 mb-4 my-4">
-            Current Balance: <strong>10</strong> ETH
+            Current Balance: <strong>{balance}</strong> ETH
           </div>
           <button className="button is-primary mr-2">Donate</button>
           <button className="button is-link is-outlined">Withdraw</button>
